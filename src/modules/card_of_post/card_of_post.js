c626f02d7286d69f9bd9608b902c359e3c685e95
shinshilkina <anastasiashilina98@gmail.com>
2020-01-17 20:01:06 +0700

    make active card

--- src/modules/card_of_post/card_of_post.js
+++ src/modules/card_of_post/card_of_post.js
@@ -1,3 +1,11 @@
 "use strict";
 import "../post/post-data.pug";
 import './card_of_post.scss';
+import $ from "jquery";
+
+
+$('.cards__item__see-more').click(function(){
+    $(".hidd").fadeToggle(100);
+    $(this).children().text( $(this).parent().is('.active') ? 'Читать полный отзыв' : 'Закрыть отзыв');
+    $(this).parent().toggleClass('active');
+});
