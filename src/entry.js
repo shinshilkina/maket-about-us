4e3ade391c99384888b3ff18c836c3cd976260b3
shinshilkina <anastasiashilina98@gmail.com>
2020-01-21 13:13:29 +0700

    create pages

--- src/entry.js
+++
@@ -1,6 +0,0 @@
-function requireAll(requireContext) {
-    return requireContext.keys().map(requireContext);
-}
-
-requireAll(require.context('./modules', true, /^\.\/(?!.*(?:__tests__)).*\.(jsx?)$/));
-requireAll(require.context('./pages', true, /^\.\/(?!.*(?:__tests__)).*\.(jsx?)$/));
--- src/modules/post/post.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-import "../post/post-data.pug";
-import './post.scss';
-import $ from "jquery";
-
